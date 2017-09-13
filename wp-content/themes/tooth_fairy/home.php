26aa74cdbba347d05f87285db6a0fb4dcbf59e95
BrieannaMann <brieanna.mann1@gmail.com>
2017-09-13 11:53:12 -0400

    changed home.php and removed all old classes

--- wp-content/themes/tooth_fairy/home.php
+++ wp-content/themes/tooth_fairy/home.php
@@ -12,20 +12,20 @@ get_header(); ?>
 			<?php while(have_posts()):the_post(); ?>
 		    	<div>
 		                <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
-		            <div class="journal-img width-100 flex vertical-direction space-between" style="background: url('<?php echo $backgroundImg[0]; ?>'); background-size:cover;">
-		                <div class="journal-name white-font green-bkg flex flex-left padding-med">
+		            <div style="background: url('<?php echo $backgroundImg[0]; ?>'); background-size:cover;">
+		                <div >
 		                    <h2> <?php the_title() ?> </h2>
 		                </div>
-		                <div class="journal-padding white-font green-bkg flex flex-right flex-align-right">
+		                <div >
 		                    <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / BY <?php the_author(); ?>
 		                </div>
 		            </div>
-		            <div class="secondary-font padding-top-med">
+		            <div>
 		                <?php the_excerpt(); ?>
 		            </div>
-		            <div class="margin-10 flex padding-bottom-lg">
-		                <?php echo '<a class="flex-center" href="' . get_permalink() . '"> '?>
-		                <h6 class="black-border black-font button-padding">READ MORE -> </h6>
+		            <div>
+		                <?php echo '<a href="' . get_permalink() . '"> '?>
+		                <h6 >READ MORE -> </h6>
 		                <?php echo '</a>'; ?>
 		            </div>
 		        </div>
