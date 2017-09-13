cb62f5f316a3e6b3c56b58362621f32260f0ba30
Joseph Dalmasso <me@jodalmasso.com>
2017-09-13 09:40:48 -0400

    started homepage

--- wp-content/themes/tooth_fairy/front-page.php
+++ wp-content/themes/tooth_fairy/front-page.php
@@ -5,4 +5,37 @@ front-page.php
 get_header();
 
 get_banner();
+
+echo '<h2>';
+echo get_field("banner_message");
+echo '</h2>';
+
+echo '<h2>';
+echo get_field("question");
+echo '</h2>';
+
+echo '<h3>';
+echo get_field("answer_one");
+echo '</h3>';
+
+echo '<h4>';
+echo get_field("reasoning_one");
+echo '</h4>';
+
+echo '<h3>';
+echo get_field("answer_two");
+echo '</h3>';
+
+echo '<h4>';
+echo get_field("reasoning_two");
+echo '</h4>';
+
+echo '<h3>';
+echo get_field("answer_three");
+echo '</h3>';
+
+echo '<h4>';
+echo get_field("reasoning_three");
+echo '</h4>';
+
 ?>
