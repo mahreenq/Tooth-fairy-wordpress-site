890d755833f632e79106acbabaa45e306f4c7519
moe-ismail <moeismail.10@gmail.com>
2017-09-15 10:50:22 -0400

    fixed the fields

--- wp-content/themes/tooth_fairy/page-faq.php
+++ wp-content/themes/tooth_fairy/page-faq.php
@@ -1,40 +1,35 @@
-<?php /* Template Name: FAQ*/ ?>
+	<?php /* Template Name: FAQ*/ ?>
 
-<?php
-/**
- * The template for displaying all single posts.
- *
- * @package RED_Starter_Theme
- */
+	<?php
+	/**
+	* The template for displaying all single posts.
+	*
+	* @package RED_Starter_Theme
+	*/
 
- get_header(); ?>
+	get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+	<div id="primary">
+	<div id="content" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post(); 
+	
+	// Array to get the FAQ Fields
 
-					<?php get_template_part( 'content', 'page' ); 
-               ?>
-                 <div>
-                 <?php  	$questions = get_post_meta(get_the_ID(), 'wiki_test_repeat_group')[0]; ?>
-                 </div>
-                    
-                  <?php  
-				
-					$answers = get_post_meta(get_the_ID(), 'wiki_test_repeat_group'[0];) 
+	<?php  	$faqs = get_post_meta(get_the_ID(), 'wiki_test_repeat_group')[0]; 
 
-					foreach ($questions as $question) {
-						var_dump($question);
-						echo $question['question'];
-					}
-					?>
+	// Acessing members of the array
 
-					<?php comments_template( '', true ); ?>
+	foreach($faqs as $faq) {
+	echo $faq['question'];
+	echo $faq['answer'];
+	}
+		
+	?>
 
-				<?php endwhile; // end of the loop. ?>
+	<?php endwhile; // end of the loop. ?>
 
-			</div><!-- #content -->
-		</div><!-- #primary -->
+	</div><!-- #content -->
+	</div><!-- #primary -->
 
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
