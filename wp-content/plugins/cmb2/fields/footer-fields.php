5116f28e8bc320cf6bebe71a951819298015bb10
moe-ismail <moeismail.10@gmail.com>
2017-09-15 14:59:18 -0400

    fixes fields

--- wp-content/plugins/cmb2/fields/footer-fields.php
+++ wp-content/plugins/cmb2/fields/footer-fields.php
@@ -1,18 +1,18 @@
 <?php
-add_action( 'cmb2_admin_init', 'cmb2_faq', '' );
+add_action( 'cmb2_admin_init', 'cmb2_footer', '' );
 /**
  * Define the metabox and field configurations.
  */
-function cmb2_faq() {
+function cmb2_footer() {
 
     // Start with an underscore to hide fields from custom fields list
-    $prefix = '_faq_';
+    $prefix = '_footer_';
 
 $cmb = new_cmb2_box( array(
-    'id'           => 'faq',
-    'title'        => 'Frequently Asked Questions?',
+    'id'           => 'footer',
+    'title'        => 'This ',
     'object_types' => array( 'page' ), // post type
-    'show_on'      => array( 'key' => 'page-template', 'value' => 'page-faq.php' ),
+    'show_on'      => array( 'key' => 'page-template', 'value' => 'footer.php' ),
     'context'      => 'normal', //  'normal', 'advanced', or 'side'
     'priority'     => 'default',  //  'high', 'core', 'default' or 'low'
     'show_names'   => true, // Show field names on the left
@@ -32,15 +32,21 @@ $group_field_id = $cmb->add_field( array(
 ) );
 
 $cmb->add_group_field( $group_field_id, array(
-    'name' => 'Questions',
-    'id'   => 'question',
+    'name' => 'Entry Image',
+    'id'   => 'footer-icon',
+    'type' => 'file',
+) );
+
+$cmb->add_group_field( $group_field_id, array(
+    'name' => 'Entry Title',
+    'id'   => 'footer-title',
     'type' => 'text',
 ) );
 
 $cmb->add_group_field( $group_field_id, array(
-    'name' => 'Answers',
-    'description' => '',
-    'id'   => 'answer',
+    'name' => 'Description',
+    'description' => 'Write a short description for this entry',
+    'id'   => 'footer-description',
     'type' => 'textarea_small',
 ) );
 
