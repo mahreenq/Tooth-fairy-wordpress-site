2934e3d4b34de96fcb584061fefb1a3757001065
Mahreen Qureshi <mahreenq@hotmail.com>
2017-09-18 11:05:04 -0400

    forms

--- wp-content/themes/tooth_fairy/template-parts/content-forms.php
+++ wp-content/themes/tooth_fairy/template-parts/content-forms.php
@@ -13,23 +13,21 @@ get_header(); ?>
 if (is_page(array('book-step-1', 'book-step-2', 'book-step-3', 'book-step-4', 'booking-confirmation'))){ ?>
 
   <?php  $bookingform = get_post_meta( get_the_ID(), 'wiki_test_repeat_group'); ?>
-  <?php foreach ($bookingform[0] as $bookingformheading) {  ?>
+  <?php foreach ($bookingform[0] as $bookingformheadings) {  ?>
     <?php
     if(sizeof($bookingform[0] >0)){
      ?>
       <h1>
-        <?php echo $bookingformheading['heading-form-1']; ?>
+        <?php echo $bookingformheadings['heading-form-1']; ?>
       </h1>
       <h2>
-        <?php echo $bookingformheading['subheading-form-1']; ?>
+        <?php echo $bookingformheadings['subheading-form-1']; ?>
       </h2>
       <h4>
-        <?php echo $bookingformheading['booking-info']; ?>
+        <?php echo $bookingformheadings['booking-info']; ?>
       </h4>
       <?php } else { ?>
         <?php echo "no content"; }} ?>
-
-
 
 
 <?php while ( have_posts() ) : the_post(); ?>
