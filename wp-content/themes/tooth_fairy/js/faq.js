961b71f0c5954e67be7fd133db0209adf661a2f8
moe-ismail <moeismail.10@gmail.com>
2017-09-18 15:41:13 -0400

    more fixes

--- wp-content/themes/tooth_fairy/js/faq.js
+++ wp-content/themes/tooth_fairy/js/faq.js
@@ -1,24 +1,14 @@
+
 var jqc = document.createElement('script');
 jqc.src = "https://rawgit.com/danielstocks/jQuery-Collapse/master/src/jquery.collapse.js";
 document.getElementsByTagName('head')[0].appendChild(jqc);
 
-$(document).ready(function() {
-    // Optimalisation: Store the references outside the event handler:
-    var $window = $(window);
-    var $pane = $('#pane1');
+jQuery(document).ready(function() {
+            if (jQuery(window).width() < 480){
+                var jqc = document.createElement('script');
+jqc.src = "https://rawgit.com/danielstocks/jQuery-Collapse/master/src/jquery.collapse.js";
+document.getElementsByTagName('head')[0].appendChild(jqc);
 
-    function checkWidth() {
-        var windowsize = $window.width();
-        if (windowsize > 440) {
-            //if the window is greater than 440px wide then turn on jScrollPane..
-            $pane.jScrollPane({
-               scrollbarWidth:15, 
-               scrollbarMargin:52
-            });
-        }
-    }
-    // Execute on load
-    checkWidth();
-    // Bind event listener
-    $(window).resize(checkWidth);
-});
+
+<script src="jquery.collapse.js"></script>
+
