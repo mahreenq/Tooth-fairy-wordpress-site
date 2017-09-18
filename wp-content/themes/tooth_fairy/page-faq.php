2155318638defb06255e48106120ba1316cddc8b
moe-ismail <moeismail.10@gmail.com>
2017-09-18 16:55:15 -0400

    fixes

--- wp-content/themes/tooth_fairy/page-faq.php
+++ wp-content/themes/tooth_fairy/page-faq.php
@@ -6,8 +6,36 @@
 	*
 	* @package RED_Starter_Theme
 	*/
-
+      
 	get_header(); ?>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+
+
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Our Services <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 	<div id="primary">
 	<div id="content" role="main" data>
@@ -41,16 +69,14 @@
 
 	</div><!-- #content -->
 	</div><!-- #primary -->
+ 
+
 
 	<?php get_footer(); ?> 
 
-    <?php 
-     wp_enqueue_script( 'faq', get_template_directory_uri() . '/build/js/faq.js', array(), true );
-
-     ?>
 
 
-
+<!--
     <script type="text/javascript">
     	
     	   jQuery(document).ready(function() {
@@ -59,11 +85,6 @@
              jqc.src = "https://rawgit.com/danielstocks/jQuery-Collapse/master/src/jquery.collapse.js";
               document.getElementsByTagName('head')[0].appendChild(jqc);
 
-
-
-
     		}
     	});
     </script>
-
-	
