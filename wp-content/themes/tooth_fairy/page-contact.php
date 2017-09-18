2ee8f594f373b9b1e3298c076f78295771b3a385
BrieannaMann <brieanna.mann1@gmail.com>
2017-09-18 14:11:47 -0400

    contact page mobile css

--- wp-content/themes/tooth_fairy/page-contact.php
+++ wp-content/themes/tooth_fairy/page-contact.php
@@ -4,27 +4,28 @@ get_header();
 
 
 while ( have_posts() ) : the_post(); ?>
-<div class="padding-lg">
+<div class="contact-padding">
 <?php the_post_thumbnail(); ?>
-</div>
+
 <?php
 $text = get_post_meta( get_the_ID(), 'wiki_test_repeat_group');
 if(sizeof($text[0]) > 0){
 foreach($text[0] as $info) { ?>
-  <div class="flex direction-column align-items-center">
-    <h2> <?php echo $info['dentist-name']; ?> </h2>
-  <div class="flex">
+  <div class="flex direction-column">
+    <h2 class="padding-bottom-sm"> <?php echo $info['dentist-name']; ?> </h2>
+  <div class="flex align-items-center">
     <div class="width-25">
-      <img  src= "<?php echo $info['phone-icon-contact'];?>">
+      <img src= "<?php echo $info['phone-icon-contact'];?>">
     </div>
-    <h4> <?php echo $info['contact-phone-number']; ?> </h4>
+    <h4 class="padding-left-sm"> <?php echo $info['contact-phone-number']; ?> </h4>
   </div>
-  <div class="flex">
+  <div class="flex align-items-center">
     <div class="width-25">
       <img  src= "<?php echo $info['email-icon-contact']; ?>">
     </div>
-    <h4> <?php echo $info['contact-email']; ?> </h4>
+    <h4 class="padding-left-sm"> <?php echo $info['contact-email']; ?> </h4>
   </div>
+</div>
 </div>
   <?php  }
   }
@@ -33,7 +34,7 @@ foreach($text[0] as $info) { ?>
 <div class="lg-blue-bkg flex direction-column align-items-center">
   <?php
       $title = get_post_meta( get_the_ID(), 'formTitle');?>
-        <h2> <?php echo $title[0]?> </h2>
+        <h2 class="padding-lg text-center"> <?php echo $title[0]?> </h2>
  <?php the_content(); ?>
  </div>
 <?php endwhile; // End of the loop. ?>
