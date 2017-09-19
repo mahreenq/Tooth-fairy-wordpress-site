d2c4c144ee530006292ac744af0c7a5031e16d14
Mahreen Qureshi <mahreenq@hotmail.com>
2017-09-19 14:33:23 -0400

    create template parts for corp page

--- wp-content/themes/tooth_fairy/page-corporate.php
+++ wp-content/themes/tooth_fairy/page-corporate.php
@@ -1,17 +1,27 @@
-<?php /* Template Name: Corporate*/ ?>
 <?php
-get_header();
-?>
-<?php
-$bannerCorp = get_post_meta( get_the_ID(), 'wiki_test_repeat_group');
-if(sizeof($bannerCorp[0]) > 0){
- foreach($bannerCorp[0] as $banner) { ?>
-   <div style="background: url('<?php echo $banner['image-banner-corp']; ?>'); background-size:cover; height:80vh;">
-     <h2> <?php echo $banner['title-banner-corp'] ?> </h2>
-      <h2> <?php echo $banner['description-banner-corp'] ?> </h2>
-   </div>
-   <?php
- } }?>
+/* Template Name: Corporate*/
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+        
+        <?php get_template_part( 'template-parts/page-templates', 'corporate' ); ?>
+
+  			<?php endwhile; // End of the loop. ?>
+
+  		</main><!-- #main -->
+  	</div><!-- #primary -->
+
 <?php
   $i = 2;
     while($i < 4) {
@@ -21,36 +31,38 @@ if(sizeof($bannerCorp[0]) > 0){
       $serviceRes = get_post_meta( get_the_ID(), 'wiki_test_repeat_group_'.$i);
 
       foreach($serviceRes[0] as $services) { ?>
-    		<div>
+    		<div class="padding-sm">
     			<img src= "<?php echo $services['image-corp']; ?>">
-    			<h3><?php echo $services['description-1'];?></h3>
-    			<p><?php echo $services['description-2']; ?></p>
-          <p><?php echo $services['description-3']; ?></p>
-          <p><?php echo $services['description-4']; ?></p>
+    			<p><span class="lg-blue-font"><?php echo $services['description-1'];?> </span>
+    			<?php echo $services['description-2']; ?>
+          <p><span class="lg-blue-font"><?php echo $services['description-3']; ?></span>
+          <?php echo $services['description-4']; ?></p>
     		</div>
     		<?php }} else if ($i===3) { ?>
 
             <div class ="lg-blue-bkg">
 
       <?php $servicesTitle = get_post_meta( get_the_ID(), 'services-corp_'.$i); ?>
-          <h3> <?php	print_r($servicesTitle[0]); ?> </h3>
+          <h3 class="padding-lg text-center"> <?php	print_r($servicesTitle[0]); ?> </h3>
        <?php $serviceRes = get_post_meta( get_the_ID(), 'wiki_test_repeat_group_'.$i);
 
       foreach($serviceRes[0] as $services) { ?>
 
+        <div class = "flex align-items-center padding-lg">
           <img src= "<?php echo $services['image-corp']; ?>">
           <div data-collapse>
-              <div>
+              <div class = "text-center">
                <h4 class="showdesc black-font"><?php echo $services['description-1'];?> <i class="fa fa-angle-down" aria-hidden="true"></i></h4>
              </div>
 
-             <div class="hidedesc">
+             <div class="hidedesc ">
                   <p ><?php echo $services['description-2']; ?></p>
                   <p ><?php echo $services['description-3']; ?></p>
                   <p ><?php echo $services['description-4']; ?></p>
             </div>
 
           </div>
+        </div>
 
 
       <?php  } ?>
@@ -63,7 +75,9 @@ if(sizeof($bannerCorp[0]) > 0){
 
 <?php
     $bookCorp = get_post_meta( get_the_ID(), 'scheduleAppCorp');?>
-          <button> <?php echo $bookCorp[0]?> </button>
+          <div class="flex justify-center padding-lg">
+          <button class="med-blue-bkg white-font"> <?php echo $bookCorp[0]?> </button>
+        </div>
         </div>
 <?php get_footer(); ?>
 
