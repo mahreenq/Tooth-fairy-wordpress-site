e57179e03778e7dc5d0bd5074ce636d6360b7bbf
moe-ismail <moeismail.10@gmail.com>
2017-09-19 11:07:44 -0400

    some more fixes

--- wp-content/themes/tooth_fairy/header.php
+++ wp-content/themes/tooth_fairy/header.php
@@ -15,10 +15,12 @@
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
 	<?php wp_head(); ?>
 	</head>
 
+	
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
@@ -36,3 +38,68 @@
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+
+	
+	<div class="mobile-nav">
+     <div class="menu-btn" id="menu-btn">
+	<div></div>
+	<span></span>
+	<span></span>
+	<span></span>
+     </div>
+ 
+     <div class="responsive-menu">
+        <ul>
+           <li>1. Hello</li>
+           <li>2. There</li>
+        </ul>
+     </div>
+</div>
+
+<script type="text/javascript">
+	jQuery(function($){
+    	$( '.menu-btn' ).click(function(){
+    		$('.responsive-menu').addClass('expand')
+    		$('.menu-btn').addClass('btn-none')
+    	})
+    	
+    	 $( '.close-btn' ).click(function(){
+    		$('.responsive-menu').removeClass('expand')
+    		$('.menu-btn').removeClass('btn-none')
+    	})
+  	})
+ </script>
+
+ <script type="text/javascript">
+	jQuery(function($){
+    	$( '.menu-btn' ).click(function(){
+    		$('.responsive-menu').addClass('expand')
+    		$('.menu-btn').addClass('btn-none')
+    	})
+    	
+    	 $( '.close-btn' ).click(function(){
+    		$('.responsive-menu').removeClass('expand')
+    		$('.menu-btn').removeClass('btn-none')
+    	})
+  	})
+ </script>
+
+
+
+ <style type="text/css">
+ 	.responsive-menu{
+max-height:0;
+background:#999999;
+width:300px;
+overflow:hidden;
+transition:all .25s;
+}
+
+.expand {
+
+max-height:300px; 
+}
+
+
+
+ </style>
