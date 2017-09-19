2e1963bdb9825027a6f1bff56e5f819c0812d920
BrieannaMann <brieanna.mann1@gmail.com>
2017-09-19 09:34:59 -0400

    changes to residential page

--- wp-content/themes/tooth_fairy/page-residential.php
+++ wp-content/themes/tooth_fairy/page-residential.php
@@ -6,18 +6,21 @@ get_header();
 $bannerRes = get_post_meta( get_the_ID(), 'wiki_test_repeat_group');
 if(sizeof($bannerRes[0]) > 0){
 	foreach($bannerRes[0] as $banner) { ?>
-		<div style="background: url('<?php echo $banner['image-banner-res']; ?>'); background-size:cover; height:80vh;">
-			<h2> <?php echo $banner['title-banner-res'] ?> </h2>
-      <h2> <?php echo $banner['description-banner-res'] ?> </h2>
+		<div class="relative">
+		<div class="res-banner " style="background: url('<?php echo $banner['image-banner-res']; ?>'); background-size:cover;">
+			<h2 class="white-font text-center"> <?php echo $banner['title-banner-res'] ?> </h2>
+		</div>
+		<h2 class="grey-font zero-bottom home-padding absolute "> <?php echo $banner['description-banner-res'] ?></h2>
 		</div>
 		<?php
 	}} ?>
+	<div class="padding-med">
 <?php
 $i = 2;
 while($i < 4) {
-  $servicesTitle = get_post_meta( get_the_ID(), 'services-residential_'.$i);
-    		print_r($servicesTitle[0]);
-  $serviceRes = get_post_meta( get_the_ID(), 'wiki_test_repeat_group_'.$i);
+  $servicesTitle = get_post_meta( get_the_ID(), 'services-residential_'.$i); ?>
+    	<h3><?php	print_r($servicesTitle[0]); ?> </h3>
+  <?php $serviceRes = get_post_meta( get_the_ID(), 'wiki_test_repeat_group_'.$i);
 	if(sizeof($serviceRes[0]) > 0){
   foreach($serviceRes[0] as $services) { ?>
 		<div>
@@ -31,6 +34,7 @@ while($i < 4) {
 	} }
   $i++;
 }?>
+</div>
 <?php
     $bookRes = get_post_meta( get_the_ID(), 'scheduleApp');?>
           <button> <?php echo $bookRes[0]?> </button>
