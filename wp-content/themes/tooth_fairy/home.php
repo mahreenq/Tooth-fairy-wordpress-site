ef907e5ec1638bf8b299d6c3b0ab64fdd48b3174
BrieannaMann <brieanna.mann1@gmail.com>
2017-09-20 10:19:40 -0400

    blog single page styling completed need to fix button

--- wp-content/themes/tooth_fairy/home.php
+++ wp-content/themes/tooth_fairy/home.php
@@ -10,7 +10,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div>
 			<?php while(have_posts()):the_post(); ?>
-		    	<div>
+		    	<div class="flex direction-column">
 		                <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
 		            <div class="height-50 width-100 d-width-75 flex direction-column justify-between" style="background: url('<?php echo $backgroundImg[0]; ?>'); background-size:cover;">
 		                <div class="med-blue-bkg flex justify-start white-font width-75 padding-med" >
@@ -20,7 +20,7 @@ get_header(); ?>
 		                   <p> <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / BY <?php the_author(); ?> </p>
 		                </div>
 		            </div>
-		            <div class="padding-lg">
+		            <div class="padding-lg d-width-75 ">
 		                <?php the_excerpt(); ?>
 		            </div>
 		            <div class="padding-lg">
