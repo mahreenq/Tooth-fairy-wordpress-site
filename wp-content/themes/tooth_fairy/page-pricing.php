cdf2eb301ee9dae28c1da653efd435650cf31eda
Joseph Dalmasso <me@jodalmasso.com>
2017-09-20 09:39:58 -0400

    added media query styling to table

--- wp-content/themes/tooth_fairy/page-pricing.php
+++ wp-content/themes/tooth_fairy/page-pricing.php
@@ -27,13 +27,13 @@ else{
 //==================Savings=======================
 echo "<div class='flex direction-row justify-between border-radius white-font med-blue-bkg padding-left-med padding-top-med padding-bottom-med'>";
 $price_category = get_post_meta( get_the_ID(), 'category-title-pricing');
-    echo "<h4>".$price_category[0]."</h4>";
+    echo "<h4 class='width-40'>".$price_category[0]."</h4>";
 
 $price_category = get_post_meta( get_the_ID(), 'our-price_title-pricing');
-    echo "<h4 class='width-25 text-center'>".$price_category[0]."</h4>";
+    echo "<h4 class='width-25 text-center width-30'>".$price_category[0]."</h4>";
 
 $price_category = get_post_meta( get_the_ID(), 'their-price-title-pricing');
-    echo "<h4 class='m-hide d-show'>".$price_category[0]."</h4>";
+    echo "<h4 class='m-hide d-show text-center width-30'>".$price_category[0]."</h4>";
 echo "</div>";
 
 $price_category = get_post_meta( get_the_ID(), 'wiki_test_repeat_group_2');
@@ -41,9 +41,9 @@ if(sizeof($price_category[0]) > 0){
     echo "<ul class='price-list lg-blue-bkg padding-left-med'>";
 foreach($price_category[0] as $main_pricing_2) {
 	echo "<li class='flex direction-row justify-between'>";
-	echo "<div class='border-right-blue width-75 padding-top-med padding-bottom-med'>".$main_pricing_2['category-pricing']."</div>";
-    echo "<div class='text-center width-25'>".$main_pricing_2['our-price-pricing']."</div>";
-    echo "<div class='m-hide d-show'>".$main_pricing_2['their-price-pricing']."</div>";
+	echo "<div class='border-right-blue width-75 padding-top-med padding-bottom-med width-40'>".$main_pricing_2['category-pricing']."</div>";
+    echo "<div class='text-center width-25 padding-top-med padding-bottom-med width-30'>".$main_pricing_2['our-price-pricing']."</div>";
+    echo "<div class='m-hide d-show padding-top-med padding-bottom-med text-center width-30 border-left-blue'>".$main_pricing_2['their-price-pricing']."</div>";
     echo "</li>";
     }
     echo "</ul>";
