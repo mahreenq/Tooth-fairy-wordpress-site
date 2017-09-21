e171a9a4b8a64f824b28eba450c9e378eb49fcb2
Joseph Dalmasso <me@jodalmasso.com>
2017-09-21 15:38:16 -0400

    pushing for review

--- wp-content/themes/tooth_fairy/js/navbar.js
+++ wp-content/themes/tooth_fairy/js/navbar.js
@@ -1,12 +1,17 @@
 var dropdown = document.getElementById('myDropdown');
 var x = document.getElementById('primary-menu');
-dropdown.addEventListener("click", function (){
+x.style.display = 'none';
+dropdown.addEventListener("click", function (e){
   console.log(x);
   console.log(dropdown);
     if (x.style.display === 'none') {
-        x.style.display = 'block';
+        x.style.display = 'flex';
     } else {
         x.style.display = 'none';
     }
 });
-console.log("hi");
+
+var sublist = document.getElementsByClassName("menu-item-has-children");
+var y = document.getElementsByClassName("sub-menu");
+
+// sublist.addEventListener("click", function())
