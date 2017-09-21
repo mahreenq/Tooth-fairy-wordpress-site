09ff4f585cebd74decc94aebcc6810f4de5b8af0
Joseph Dalmasso <me@jodalmasso.com>
2017-09-21 10:21:18 -0400

    optimizing cart

--- wp-content/themes/tooth_fairy/template-parts/page-templates/search-bar.php
+++ wp-content/themes/tooth_fairy/template-parts/page-templates/search-bar.php
@@ -10,31 +10,28 @@ foreach ($auto[0] as $a) {
 }
 ?>
 
-<div class="ui-widget flex direction-column padding-bottom-xlg">
+<div class="ui-widget flex direction-column padding-bottom-xlg text-center">
     <div class="flex direction-row justify-center padding-md">
       <input id="tags" class="width-100 d-width-25rem border-blue padding-sm" style="background-image: url('http://localhost:8888/tooth_fairy/wp-content/themes/tooth_fairy/assets/search.svg'); background-size: 2rem; background-repeat: no-repeat; background-position: 98%;">
     </div>
-  <div id="result"></div>
+  <div id="result"><p id="accept" class="green-font"></p></div>
 </div>
 
   <script>
     var providers = <?php echo "[".$providers."]"; ?>;
     $( "#tags" ).autocomplete({
       source: providers,
-      response: function(event, ui){
-        if (ui.content.length === 0) {
-            $("#result").html('<p class="red-font">' + "No results found" + '</p>');
-        } else {
-            $("#result").html('<p class="green-font">' + "We accept plans from" + " " + '<span id="accept"></span>' + '</p>');
-        }
-      }
     });
 
     $(document).ready(function () {
         $(".ui-autocomplete").on("click",function (e){
-            $("#result").show();
-            var inputVal = $("#tags").val()
-            $("#accept").text(inputVal);
+            var inputVal = $("#tags").val();
+            if (inputVal == 0){
+              $("#accept").html("No results found!");
+            }
+            else{
+              $("#accept").html("We accept plans from" + " " + inputVal + ".");
+            }
         })
     })
 
