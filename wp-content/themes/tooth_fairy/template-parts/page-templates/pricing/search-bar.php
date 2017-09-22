56b06322507258af443e685e9cf694a652ceb86a
BrieannaMann <brieanna.mann1@gmail.com>
2017-09-22 09:53:27 -0400

    Search bar working

--- wp-content/themes/tooth_fairy/template-parts/page-templates/pricing/search-bar.php
+++ wp-content/themes/tooth_fairy/template-parts/page-templates/pricing/search-bar.php
@@ -10,8 +10,6 @@ foreach ($auto[0] as $a) {
 }
 ?>
 
-<div class="width-100 height-100" style="background-image: url('<?php echo get_template_directory_uri().'/assets/search.png'?>')contain no-repeat ;">
-</div>
 <div class="ui-widget flex direction-column padding-bottom-xlg text-center">
     <div class="flex direction-row justify-center padding-md">
       <input id="tags" class="width-100 d-width-25rem border-blue padding-sm" style="background-image: url('<?php echo get_template_directory_uri().'/assets/search.png'?>'); background-size: 2rem; background-repeat: no-repeat; background-position: 98%;">
@@ -25,7 +23,7 @@ foreach ($auto[0] as $a) {
 
   <script>
 
-
+jQuery(document).ready(function($){
     var providers = <?php echo "[".$providers."]"; ?>;
     $( "#tags" ).autocomplete({
       source: providers,
@@ -38,6 +36,8 @@ foreach ($auto[0] as $a) {
             $("#accept").html("We accept plans from" + " " + inputVal + ".");
         });
     })
-
+})
   </script>
-
+  <link href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" rel="Stylesheet"></link>
+<script src="YourJquery source path"></script>
+<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js" ></script>
